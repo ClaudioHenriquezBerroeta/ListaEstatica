70f3f83ac8d274d85871fa125e8a483881b25bb5
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-05-12 13:24:01 -0400

    Update lista_estatica.c

--- lista_estatica.c
+++ lista_estatica.c
@@ -41,18 +41,61 @@ void mostrar(struct lista *L){
 		printf("%i - ", L->arreglo[i]);
 	printf("\n");
 }
+struct lista *eliminar(int p,struct lista *L){
+	int i;
+	if(L->ultimo==0){//lista no tiene elemento
+		printf("Lista vacia, no es posible eliminar!");
+	}else{
+		if((p<0)||(p>=L->ultimo)){
+			printf("Posicion invalida, no es posible eliminar!");
+		}else{
+			for(i=p;i<L->ultimo-1;i++){
+				L->arreglo[i]=L->arreglo[i+1];
+			}
+			L->ultimo--;
+		}
+	}
+	return L;
+}
+int localizar(int x, struct lista *L){
+	int i;
+	for(i=0;i<L->ultimo;i++){
+		if(x==L->arreglo[i]){
+			printf("La posicion del elemento %i es : %i \n",x,i);
+			return i;
+		}
+	}
+	printf("El valor no se encuentra!");
+	return -1;
+}
 
+int recuperar(int p, struct lista *L){
+	if((p<0)||(p>=L->ultimo)){
+		printf("La posicion no es valida!\n");
+		return -1;
+	}else{
+		printf("El elmento de la posicion %i es : %i \n",p,arreglo[p]);
+		return L->arreglo[p];
+	}
+}
 int main(int argc, char const *argv[])
 {
 	/* code */
-	struct lista *Lista1; //declaramos la lista
-	Lista1 = constructor(); //creamos la lista, asignar memoria (instanciar)
-	Lista1 = insertar(100,0,Lista1);
-	Lista1 = insertar(200,0,Lista1);
-	mostrar(Lista1);
+	struct lista *L; //declaramos la lista
+	L = constructor(); //creamos la lista, asignar memoria (instanciar)
+	//L=eliminar(0,L);
+	L=insertar(10,0,L);
+	L=insertar(20,1,L);
+	L=insertar(30,2,L);
+	L=insertar(40,3,L);
+	mostrar(L);
+	L=eliminar(2,L);
+	mostrar(L);
+	localizar(30,L);
+	recuperar(-1,L);
 	//printf("%i\n",Lista1->arreglo[10]);
 	//printf("%i\n",Lista1->arreglo[20]);	
-	destructor(Lista1); // liberamos memoria
+	destructor(L); // liberamos memoria
 
 	return 0;
 }
