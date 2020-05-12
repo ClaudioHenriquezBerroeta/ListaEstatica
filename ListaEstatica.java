a82cb25a428f672dd601823b6a34837f3f15ea54
Claudio Henríquez <63313873+ClaudioHenriquezBerroeta@users.noreply.github.com>
2020-05-12 12:19:51 -0400

    Update ListaEstatica.java

--- ListaEstatica.java
+++ ListaEstatica.java
@@ -4,7 +4,7 @@ public class ListaEstatica{
 	final int long_max;
 
 	ListaEstatica(int n){
-		if(n>=0)long_max=n;
+		if(n>0)long_max=n;
 		else long_max=200;
 
 		arreglo= new int[long_max];
@@ -31,11 +31,30 @@ public class ListaEstatica{
 		}
 		System.out.println('\b');
 	}
+	void eliminar(int p){
+		if(ultimo==0){//lista no tiene elemento
+			System.out.println("Lista vacia, no es posible eliminar!");
+		}else{
+			if((p<0)||(p>=ultimo)){
+				System.out.println("Posicion invalida, no es posible eliminar!");
+			}else{
+				for(int i=p;i<ultimo-1;i++){
+					arreglo[i]=arreglo[i+1];
+				}
+				ultimo--;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		ListaEstatica L = new ListaEstatica(10);
+		L.eliminar(0);
+		L.insertar(10,0);
 		L.insertar(10,1);
-		L.insertar(20,0);
+		L.insertar(30,2);
+		L.insertar(10,3);
+		L.mostrar();
+		L.eliminar(2);
 		L.mostrar();
 	}
 }
